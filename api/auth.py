27b4f0e8d1f948d1cfc83d9ec151e6cbfd648378
Soub9977 <soubhagyashirur02@gmail.com>
2025-05-29 21:58:03 +0530

    git issue fixed

--- api/auth.py
+++ api/auth.py
@@ -50,3 +50,4 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
     access_token = create_access_token(data={"sub": user.email})
     return {"access_token": access_token, "token_type": "bearer"}
 
+#auth file 
